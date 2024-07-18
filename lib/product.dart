d3354d08dd2e8754cf12a057d7931ee71098a4ce
MuhammadNiam529 <misbahudin1428@gmail.com>
2024-07-18 21:48:06 +0700

    Menambahkan model Produk: Mendefinisikan kelas Product dengan field dan metode yang diperlukan.

--- lib/product.dart
+++ lib/product.dart
@@ -4,4 +4,13 @@ class Product {
   final String price;
 
   Product({required this.image, required this.title, required this.price});
+
+  // Fungsi factory untuk membuat instance Product dari dokumen Firestore
+  factory Product.fromFirestore(Map<String, dynamic> data) {
+    return Product(
+      image: data['image'] ?? '',
+      title: data['title'] ?? '',
+      price: data['price'] ?? '',
+    );
+  }
 }
